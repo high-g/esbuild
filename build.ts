bc8229e28d142b72ac25571a4cbfc6072f92d61c
fumiya <altema1217bbfumi@live.jp>
2021-09-28 18:18:19 +0900

    feat: react

---
+++ build.ts
@@ -0,0 +1,19 @@
+const { argv } = require('process')
+const { build } = require('esbuild')
+const path = require('path')
+
+const options = {
+  define: { 'process.env.NODE_ENV': process.env.NODE_ENV },
+  entryPoints: [path.resolve(__dirname, 'src/Index.tsx')],
+  minify: argv[2] === 'productions',
+  bundle: true,
+  target: 'es2016',
+  platform: 'browser',
+  outdir: path.resolve(__dirname, 'dist'),
+  tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+}
+
+build(options).catch((err) => {
+  process.stderr.write(err.stderr)
+  process.exit(1)
+})
